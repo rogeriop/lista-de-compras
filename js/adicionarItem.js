8a7e769e687e731ddb5231ad56c16b5f0ea0b47a
rogeriop <pinheiroxrogerio@gmail.com>
2024-10-04 20:42:20 -0300

    04 Inserindo feedbacks visuais

---
+++ js/adicionarItem.js
@@ -0,0 +1,15 @@
+import { criarItemDaLista } from "./criarItemDaLista.js";
+import { verificarListaVazia } from "./verificarListaVazia.js";
+
+const item = document.getElementById("input-item");
+const listaDeCompras = document.getElementById("lista-de-compras");
+
+
+export function adicionarItem(evento) {
+    evento.preventDefault();
+    
+    const itemDaLista = criarItemDaLista(item.value);
+    listaDeCompras.appendChild(itemDaLista);
+    verificarListaVazia(listaDeCompras);
+
+}
---
+++ js/criarItemDaLista.js
@@ -0,0 +1,91 @@
+
+const listaComprados = document.getElementById("lista-comprados");
+let contador = 0;
+
+
+export function criarItemDaLista(item) {
+    const itemDaLista = document.createElement("li");
+    const containerItemLista = document.createElement("div");
+    containerItemLista.classList.add("item-lista-container");
+    
+    const containerNomeDoItem = document.createElement("div");
+    containerNomeDoItem.classList.add("container-nome-compra");
+    
+    const containerCheckbox = document.createElement("div");
+    containerCheckbox.classList.add("checkbox-container");
+    
+    const checkBoxInput = document.createElement("input");
+    checkBoxInput.type = "checkbox";
+    checkBoxInput.classList.add("checkbox-input");
+    checkBoxInput.id = "checkbox-" + contador++;
+    
+    const checkboxLabel = document.createElement("label");
+    checkboxLabel.setAttribute("for", checkBoxInput.id);
+    
+    checkboxLabel.addEventListener("click", function(evento) {
+        const checkboxInput = evento.currentTarget.querySelector(".checkbox-input");
+        const checkboxCustomizado = evento.currentTarget.querySelector(".checkbox-customizado");
+        const itemTitulo = evento.currentTarget.closest("li").querySelector("#item-titulo");
+        if(checkboxInput.checked) {
+            checkboxCustomizado.classList.add("checked");
+            itemTitulo.style.textDecoration = "line-through";
+            listaComprados.appendChild(itemDaLista);
+        } else {
+            checkboxCustomizado.classList.remove("checked"); 
+            itemTitulo.style.textDecoration = "none";
+            listaDeCompras.appendChild(itemDaLista);
+            
+        }
+    })
+    
+    
+    const checkboxCustomizado = document.createElement("div");
+    checkboxCustomizado.classList.add("checkbox-customizado");
+
+    checkboxLabel.appendChild(checkBoxInput);
+    checkboxLabel.appendChild(checkboxCustomizado);
+    
+    containerCheckbox.appendChild(checkboxLabel);
+    containerNomeDoItem.appendChild(containerCheckbox);
+    
+    const nomeDoItem = document.createElement("p");
+    nomeDoItem.id = "item-titulo";
+    nomeDoItem.innerText = item;
+    containerNomeDoItem.appendChild(nomeDoItem);
+    
+    const containerBotoes = document.createElement("div");
+    const botaoRemover = document.createElement("button");
+    botaoRemover.classList.add("botao-acao");
+    
+    const imagemRemover = document.createElement("img");
+    imagemRemover.src = "img/delete.svg";
+    imagemRemover.alt = "Remover";
+    
+    botaoRemover.appendChild(imagemRemover);
+    
+    const botaoEditar = document.createElement("button");
+    botaoEditar.classList.add("botao-acao");
+    const imagemEditar = document.createElement("img");
+    imagemEditar.src = "img/edit.svg";
+    imagemEditar.alt = "Editar";
+    
+    botaoEditar.appendChild(imagemEditar);
+    
+    containerBotoes.appendChild(botaoRemover);
+    containerBotoes.appendChild(botaoEditar);
+    
+    const itemData = document.createElement("p");
+    itemData.innerText = `${new Date().toLocaleDateString("pt-BR", { weekday: "long"})} (${new Date().toLocaleDateString()}) às ${new Date().toLocaleTimeString("pt-BR", {hour: "numeric", minute: "numeric"} )}`;
+    itemDaLista.classList.add("item-lista-texto");
+    
+    
+    itemDaLista.appendChild(containerItemLista);
+    //containerItemLista.appendChild(itemData);
+    
+    containerItemLista.appendChild(containerNomeDoItem);
+    containerItemLista.appendChild(containerBotoes);
+    itemDaLista.appendChild(containerItemLista);
+    
+    itemDaLista.appendChild(itemData);
+    return itemDaLista;
+}
